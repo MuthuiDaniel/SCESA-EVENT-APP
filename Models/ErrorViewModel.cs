bf5cfb4a52bcf47890180b978141eb5be7952947
sanii <sanii@bountyhunter>
2024-09-02 11:54:00 +0300

    created scesaevent .net8 App

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SCESAEventApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
